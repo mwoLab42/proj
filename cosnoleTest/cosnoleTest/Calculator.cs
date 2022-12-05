797c24641a062c4acae9415c1195ae9796bdc34c
Rafał Sulowski <48453164+rafalsulowski@users.noreply.github.com>
2022-12-05 01:52:58 +0100

    initial

---
+++ cosnoleTest/cosnoleTest/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cosnoleTest
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return b != 0 ? a / b : throw new ArgumentException();
+        }
+    }
+}
